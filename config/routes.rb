6b4ce91838fb489c13afb6c2069bcfdcd5b1c0f4
yubinex <janhauck99@gmail.com>
2025-08-15 15:29:21 +0200

    feat: Add delete functionality for projects
    This commit introduces the ability to delete projects.
    
    - Adds a `destroy` action to the `ProjectsController`.
    - Adds a "Delete Project" button to the projects `show` view.
    - Refactors the project routes to use `resources :projects`.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
-
-  get "/projects", to: "projects#index"
-  get "/projects/new", to: "projects#new", as: "new_project"
-  get "/projects/:id", to: "projects#show", as: "project"
-  get "/projects/:id/edit", to: "projects#edit", as: "edit_project"
-  post "/projects", to: "projects#create"
-  patch "/projects/:id", to: "projects#update"
+  resources :projects
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
